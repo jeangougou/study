c6269ac09b65dd83979e62779fcb460a73a74bfa
Jean <jeangougou@gmail.com>
2019-01-30 19:59:34 +0000

    added functional idiomatic quicksort

--- LearningFsharp/DataStructsAndAlgos.Tests/Tests.fs
+++ LearningFsharp/DataStructsAndAlgos.Tests/Tests.fs
@@ -11,3 +11,11 @@ let ``Sorting with recursive quicksort`` () =
   let actualSort = Sorting.QuickSort data
   let expectedSort = List.sort data
   List.zip actualSort expectedSort |> List.map (fun (a,b) -> Assert.Equal(a, b))
+
+[<Fact>]
+let ``Sorting with recursive idiomatic quicksort`` () =
+  let rand = new System.Random()
+  let data = List.init 10 (fun _ -> rand.Next())
+  let actualSort = Sorting.IdiomaticFunctionalQuickSort data |> Seq.toList
+  let expectedSort = List.sort data
+  List.zip actualSort expectedSort |> List.map (fun (a,b) -> Assert.Equal(a, b))
--- LearningFsharp/DataStructsAndAlgos/Sorting.fs
+++ LearningFsharp/DataStructsAndAlgos/Sorting.fs
@@ -5,3 +5,10 @@ let rec QuickSort = function
   | n::ns -> let lessthan, gr8terEqual = List.partition ((>) n) ns
              QuickSort lessthan @ n :: QuickSort gr8terEqual
 
+let rec IdiomaticFunctionalQuickSort (pxs:seq<_>) =
+  seq {
+    match Seq.toList pxs with
+    | p::xs -> let lessthan, greaterThan = List.partition ((>=) p) xs
+               yield! IdiomaticFunctionalQuickSort lessthan; yield p; yield! IdiomaticFunctionalQuickSort greaterThan
+    | _ -> ()
+  }
