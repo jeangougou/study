ac6905b9e74cd48b5d5db8e8971a379b02f3b008
Jean <jeangougou@gmail.com>
2019-01-31 10:15:40 +0000

    bubble sort with swap by reference utils

--- LearningFsharp/DataStructsAndAlgos.Tests/Tests.fs
+++ LearningFsharp/DataStructsAndAlgos.Tests/Tests.fs
@@ -19,3 +19,12 @@ let ``Sorting with recursive idiomatic quicksort`` () =
   let actualSort = Sorting.IdiomaticFunctionalQuickSort data |> Seq.toList
   let expectedSort = List.sort data
   List.zip actualSort expectedSort |> List.map (fun (a,b) -> Assert.Equal(a, b))
+
+
+[<Fact>]
+let ``Sorting with bubblesort`` () =
+  let rand = new System.Random()
+  let data = List.init 10 (fun _ -> rand.Next())
+  let actualSort = Sorting.BubbleSort (data |> List.toArray) |> Seq.toList
+  let expectedSort = List.sort data
+  List.zip actualSort expectedSort |> List.map (fun (a,b) -> Assert.Equal(a, b))
--- LearningFsharp/DataStructsAndAlgos/Sorting.fs
+++ LearningFsharp/DataStructsAndAlgos/Sorting.fs
@@ -12,3 +12,18 @@ let rec IdiomaticFunctionalQuickSort (pxs:seq<_>) =
                yield! IdiomaticFunctionalQuickSort lessthan; yield p; yield! IdiomaticFunctionalQuickSort greaterThan
     | _ -> ()
   }
+
+let swap (left : 'a  byref) (right: 'a byref) =
+  let temp = left
+  left <- right
+  right <- temp
+
+let BubbleSort array =
+  let rec loop (array : 'arr []) =
+    let mutable swaps = 0
+    for i = 0 to array.Length - 2 do
+      if array.[i] > array.[i+1] then
+        swap &array.[i] &array.[i+1]
+        swaps <- swaps+1
+    if swaps > 0 then loop array else array
+  loop array
